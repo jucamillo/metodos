156aa14ee0f321ec10ed028bde1002f4ad93a1e1
php 04 <php.04@xuxu.cec.ime.usp.br>
2018-01-23 22:30:50 -0200

    metodos da classe conta

--- conta.php
+++ conta.php
@@ -2,10 +2,24 @@
 
 class Conta
 {
-  public $numero; //atributo
+  public $numero = 122121; //atributo
   public $saldo = 0; //atributo
+  public $limite = 100; //atributo
+  public $agencia = 232; //atributo
 
   function deposita(float $valor){
     $this->saldo += $valor;
   }
+  function saca(float $valor){
+    $this->saldo -= $valor;
+  }
+  function imprimeExtrato(){
+    echo "Agencia: $this->agencia" . PHP_EOL;
+    echo "Numero: $this->numero" . PHP_EOL;
+    echo "Saldo: $this->saldo" . PHP_EOL;
+  }
+  function consultaSaldo(){
+    echo "Saldo Disponível: $this->saldo" . PHP_EOL;
+  }
+
 }
--- testaConta.php
+++ testaConta.php
@@ -6,5 +6,16 @@ require 'conta.php';
 $conta = new Conta();
 echo "Saldo inicial: $conta->saldo" . PHP_EOL;
 
-$conta->deposita('2000 maçãs');
-echo "Saldo final: $conta->saldo" . PHP_EOL;
+$conta->deposita(2000);
+echo "Saldo depois deposito: $conta->saldo" . PHP_EOL;
+
+
+$conta->saca(100);
+echo "Saldo depois sacar: $conta->saldo" . PHP_EOL;
+
+echo "Extrato" . PHP_EOL;
+$conta->imprimeExtrato();
+
+
+echo "Consulta saldo" . PHP_EOL;
+$conta->consultaSaldo();
